082d2ae4ee7ed91eef864f12608b829e4f48c623
grantbrokergenius <grant.harding@brokergenius.com>
2019-08-17 00:32:49 -0400

    add link icons to stubhub

--- src/components/StubHubEvent.jsx
+++ src/components/StubHubEvent.jsx
@@ -3,13 +3,18 @@ import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import { ListItemSecondaryAction, IconButton } from '@material-ui/core';
+import LaunchIcon from '@material-ui/icons/Launch';
+import DoneOutlineIcon from '@material-ui/icons/DoneOutline';
 import EventContext from '../context/EventContext';
 import date from '../utils/date';
+
+const openLink = (id) => window.open(`https://stubhub.com/event/${id}`, '_blank')
 
 function StubHubEvent({
   stubhub_event_id, event_name, venue_name, event_date,
 }) {
-  const { activeStubHubEventId, setActiveStubHubEventId } = useContext(EventContext);
+  const { activeStubHubEventId, setActiveStubHubEventId, activeEventId } = useContext(EventContext);
 
   const handleClick = () => setActiveStubHubEventId(stubhub_event_id);
 
@@ -18,6 +23,18 @@ function StubHubEvent({
   return (
     <ListItem selected={selected} button key={stubhub_event_id} onClick={handleClick}>
       <ListItemText primary={event_name} secondary={`${venue_name} || ${date(event_date)}`} />
+      <ListItemSecondaryAction>
+        {activeEventId
+        && (
+        <IconButton aria-label="map">
+          <DoneOutlineIcon />
+        </IconButton>
+        )}
+
+        <IconButton aria-label="open stubhub" onClick={() => openLink(stubhub_event_id)}>
+          <LaunchIcon />
+        </IconButton>
+      </ListItemSecondaryAction>
     </ListItem>
   );
 }
