009850f7d46cf3ab748adabc5221352adb8ef9d6
Grant Harding <gharding@gmail.com>
2019-09-04 02:20:13 -0400

    delayed search input -- need to test

---
+++ src/components/SearchInput.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function SearchInput({ onChange, label, value, delay }) {
+
+
+  const [newValue, setNewValue] = useState(value);
+
+  const [timer, setTimer] = useState(null);
+
+  const handleChangeEvent = (e) => {
+    setNewValue(e.target.value);
+    if (timer) { clearImmediate(timer); }
+    setTimer(setTimeout(() => onChange(e), delay));
+  }
+
+  return (<TextField
+            style={{ flexFlow: '0 1 auto' }}
+            label={label}
+            value={newValue}
+            onChange={handleChangeEvent}
+            margin="normal"
+          />)
+
+}
+
+SearchInput.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  label: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  delay: PropTypes.number.isRequired,
+}
+
+export default SearchInput;
