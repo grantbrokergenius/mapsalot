b77db2cc1ff9fcb5e32091a2e1ef286d05bba10f
grantbrokergenius <grant.harding@brokergenius.com>
2019-09-11 22:35:12 -0400

    fix event search

--- src/events/event.js
+++ src/events/event.js
@@ -28,30 +28,34 @@ const twoyears = () => format(addDays(new Date(), 750), 'yyyy-MM-dd');
 
 const find = ({
   date_from = yesterday(), date_to = twoyears(), event_name = '%', venue_name = '%',
-}, limit = 100, offset = 0, order = 'event_date', dir = 'asc') => allowedSort.includes(order) && ['asc', 'desc'].includes(dir) && db.raw(`SELECT bg_events.bg_event_id as bg_event_id, event_name, venue_name, event_date, pos_name
-FROM \`bg_events\`
-LEFT JOIN \`event_mapping_flags\`
-ON \`bg_events\`.\`bg_event_id\` = \`event_mapping_flags\`.\`bg_event_id\`
-LEFT JOIN \`event_mappings\`
-ON \`bg_events\`.\`bg_event_id\` = \`event_mappings\`.\`bg_event_id\`
-AND \`event_mappings\`.\`exchange_id\` = 1
-LEFT JOIN \`unresolveable_mappings\`
-ON \`event_mappings\`.\`bg_event_id\` = \`unresolveable_mappings\`.\`bg_event_id\`
-AND \`event_mappings\`.\`exchange_id\` = \`unresolveable_mappings\`.\`exchange_id\`
-WHERE \`unresolveable_mappings\`.\`bg_event_id\` IS NULL
-AND \`bg_events\`.\`event_date\` BETWEEN ? AND ?
+} = {}, limit = 100, offset = 0, order = 'event_date', dir = 'asc') => allowedSort.includes(order)
+  && ['asc', 'desc'].includes(dir)
+  && db.raw(`SELECT bg_events.bg_event_id as bg_event_id, event_name, venue_name, event_date, pos_name
+    FROM \`bg_events\`
+    LEFT JOIN \`event_mapping_flags\`
+    ON \`bg_events\`.\`bg_event_id\` = \`event_mapping_flags\`.\`bg_event_id\`
+    LEFT JOIN \`event_mappings\`
+    ON \`bg_events\`.\`bg_event_id\` = \`event_mappings\`.\`bg_event_id\`
+    AND \`event_mappings\`.\`exchange_id\` = 1
+    LEFT JOIN \`unresolveable_mappings\`
+    ON \`event_mappings\`.\`bg_event_id\` = \`unresolveable_mappings\`.\`bg_event_id\`
+    AND \`event_mappings\`.\`exchange_id\` = \`unresolveable_mappings\`.\`exchange_id\`
+    WHERE \`unresolveable_mappings\`.\`bg_event_id\` IS NULL
+    AND \`bg_events\`.\`event_date\` BETWEEN ? AND ?
 
-AND event_name LIKE ?
-AND venue_name LIKE ?
-AND (
-\`event_mappings\`.\`approved\` = 0
-OR \`event_mappings\`.\`approved\` IS NULL
-OR \`event_mappings\`.\`exchange_id\` IS NULL
-) ORDER BY ${order} ${dir} LIMIT ${limit} OFFSET ${offset}`, [date_from, date_to, event_name, venue_name]).then((res) => res[0]);
+    AND event_name LIKE ?
+    AND venue_name LIKE ?
+    AND (
+    \`event_mappings\`.\`approved\` = 0
+    OR \`event_mappings\`.\`approved\` IS NULL
+    OR \`event_mappings\`.\`exchange_id\` IS NULL
+    ) ORDER BY ${order} ${dir} LIMIT ${limit} OFFSET ${offset}`,
+  [date_from, date_to, event_name, venue_name])
+    .then((res) => res[0]);
 
 const findSome = auth((_, ...args) => find(...args));
 
-const findAll = auth(() => find());
+const findAll = auth(() => find({}));
 
 /*
 $sql = <<<SQL
