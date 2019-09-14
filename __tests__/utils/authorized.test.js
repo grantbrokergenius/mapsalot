872020baead62f583c58b02e8f3c3cc6acdfedb4
grantbrokergenius <grant.harding@brokergenius.com>
2019-09-14 13:46:42 -0400

    auth test

--- __tests__/utils/authorized.test.js
+++ __tests__/utils/authorized.test.js
@@ -1,17 +1,14 @@
 import { auth, authWithUser } from '../../src/utils/authorized';
 
-describe('auth', () => {
-
-});
-
 describe('authWithUser', () => {
   test('with user', () => {
     const fn = jest.fn();
-    const ctx = { session: { user: true } };
+    const user = { name: 'grant' };
+    const ctx = { session: { user } };
 
     const authFunc = authWithUser(fn);
-    authFunc(ctx, authFunc);
-    expect(fn).toHaveBeenCalled();
+    authFunc(ctx, 'hey');
+    expect(fn).toHaveBeenCalledWith(user, 'hey');
   });
 
   test('without user', () => {
@@ -19,6 +16,27 @@ describe('authWithUser', () => {
     const ctx = { session: {} };
 
     const authFunc = authWithUser(fn);
+    authFunc(ctx, authFunc);
+    expect(fn).not.toHaveBeenCalled();
+  });
+});
+
+describe('auth', () => {
+  test('with user', () => {
+    const fn = jest.fn();
+    const user = { name: 'grant' };
+    const ctx = { session: { user } };
+
+    const authFunc = auth(fn);
+    authFunc(ctx, 'hey');
+    expect(fn).toHaveBeenCalledWith('hey');
+  });
+
+  test('without user', () => {
+    const fn = jest.fn();
+    const ctx = { session: {} };
+
+    const authFunc = auth(fn);
     authFunc(ctx, authFunc);
     expect(fn).not.toHaveBeenCalled();
   });
