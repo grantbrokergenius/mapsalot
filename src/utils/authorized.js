1b1d6da5faa112734a6c12594e2e9e2eee1dd27f
grantbrokergenius <grant.harding@brokergenius.com>
2019-09-14 13:47:50 -0400

    auth cleanup

--- src/utils/authorized.js
+++ src/utils/authorized.js
@@ -1,5 +1,6 @@
 
-const authWithUser = (func) => ({ session }, ...args) => session && session.user && func(session.user, ...args);
+const authWithUser = (func) => ({ session }, ...args) => session
+    && session.user && func(session.user, ...args);
 
 const auth = (func) => authWithUser((_, ...args) => func(...args));
 
